19c8431f312522b2b8a3abb087c12ac6cb2d045a
Roie <roihemo32321@gmail.com>
2024-02-03 14:28:06 +0200

    finished user-list styles + some changes for app.component such as routes added to the application.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  { path: '**', redirectTo: 'users' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user-model';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent {
+  users: User[] = [];
 
+  constructor() {}
 }
