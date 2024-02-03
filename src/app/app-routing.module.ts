783712168d122039430c8e2a0678eb6acd154943
Roie <roihemo32321@gmail.com>
2024-02-03 17:02:01 +0200

    added shared styles by mixins in scss and finished user-details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { NewUserComponent } from './components/new-user/new-user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'createUser', component: NewUserComponent },
+  { path: 'userDetails/:id', component: UserDetailsComponent },
   { path: '**', redirectTo: 'users' },
 ];
 
--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/models/user-model';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  styleUrls: ['./user-details.component.scss'],
 })
 export class UserDetailsComponent {
+  @Input() user: User = new User();
+  actionType: string = '';
+  editable: boolean = false;
 
+  onFieldChange(event: Event, eventName: 'name' | 'email' | 'phone') {
+    if (this.user && (<HTMLInputElement>event.target).value.length <= 50) {
+      this.user[eventName] = (<HTMLInputElement>event.target).value;
+    }
+  }
+
+  toggleAction(actionType: string) {
+    this.actionType = actionType;
+  }
 }
