bffbaf58349c3194e908abbb93a43cab153b17ff
Roie <roihemo32321@gmail.com>
2024-02-03 13:06:04 +0200

    Added all components to the folder, now starting to work on header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent,
+    NewUserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
