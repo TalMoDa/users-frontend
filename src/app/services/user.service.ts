bba77ad8988d1bd70dabfacdf810f3208d4968f9
Roie <roihemo32321@gmail.com>
2024-02-03 17:08:46 +0200

    simple user service and added template functions

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user-model';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  constructor() {}
+
+  public getUsers() {}
+  public getUserById() {}
+  public updateUser() {}
+  public createUser() {}
+  public deleteUser() {}
+}
